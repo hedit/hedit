33e33ec4c4e6615bdcda576a782c9a47d5fd01d5
Deltanic <rdg94@live.nl>
2011-05-09 19:08:11 +0000

    Ill try this >_<

---
+++ functions/mainMenu/menuSwitch.lua
@@ -0,0 +1,66 @@
+--|| ***************************************************************************************************************** [[
+--|| PROJECT:		MTA Ingame Handling Editor
+--|| FILE:			functions/menu/menuSwitch.lua
+--|| DEVELOPERS:	Remi-X <rdg94@live.nl>
+--|| PURPOSE:		Showing and updating the correct menus
+--||
+--|| COPYRIGHTED BY REMI-X
+--|| YOU ARE NOT ALLOWED TO MAKE MIRRORS OR RE-RELEASES OF THIS SCRIPT WITHOUT PERMISSION FROM THE OWNERS
+--|| THIS RESOURCE HAS BEEN UPLOADED TO COMMUNITY.MTASA.COM
+--|| ***************************************************************************************************************** ]]
+function updateData ( m )
+	if not m then m			= cm end
+	local veh				= getPedOccupiedVehicle ( localPlayer )
+	if veh then
+		local config		= getVehicleHandling ( veh )
+		for i=1,#hData[m].h do
+			if comboItem[ hData[m].h[i] ] then
+				local cI=-1
+				for v=1,#comboItem[ hData[m].h[i] ] do 
+					if config[ hData[m].h[i] ] == comboItem[ hData[m].h[i] ][v] then cI = v-1 end end
+				guiComboBoxSetSelected ( hedit[i], cI )
+			elseif hData[m].h[i] == hProperty[5] then
+				local com	= config[ hData[m].h[i] ]
+				guiSetText	(
+							hedit[i],
+							tostring	( round(com[1]) ) ..", "
+							..tostring	( round(com[2]) ) ..", "
+							..tostring	( round(com[3]) )
+							)
+			else
+				guiSetText ( hedit[i], round ( config[ hData[m].h[i] ] ) )
+			end
+		end
+		outputChatBox ( packHandling ( veh ) )
+	end
+end
+-------------------------------------------------------------------------------------------------------------------------
+-- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////// --
+-------------------------------------------------------------------------------------------------------------------------
+function showData ( m )
+	if guiGetVisible ( mainWnd.window ) then
+		cm=m
+		destroyMenuChildren ( )
+		hideUtilMenu ( )
+		if m == mProperty[4] then
+		elseif m == mProperty[5] then
+		else
+			for i=1,#hData[m].h do
+				local gX, gY = guiGetPosition	( defHedit[i], false )
+				local wX, wY = guiGetSize		( defHedit[i], false )
+				guiSetText ( label[i], iProperty[ hData[m].h[i] ][1] )
+				guiSetVisible ( label[i], true )
+				if comboItem[ hData[m].h[i] ] then
+					hedit[i] = guiCreateComboBox ( gX, gY, wX, (20*#comboItem[ hData[m].h[i] ])+20, "", false, mainWnd.window )
+					for cI=1,#comboItem[ hData[m].h[i] ] do
+						guiComboBoxAddItem ( hedit[i], comboItem[ hData[m].h[i] ][cI] )
+					end
+				else
+					hedit[i] = guiCreateButton ( gX, gY, wX, wY, "", false, mainWnd.window )
+				end
+				hButton [ hedit[i] ] = i
+			end
+			updateData ( m )
+		end
+	end
+end	 
