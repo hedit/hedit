b27ec7837f780e2540e5a01e2890626a9efcbf49
Deltanic <rdg94@live.nl>
2011-06-09 20:02:34 +0000

    Applied confirmation windows. Hedit now asks you to overwrite existing saves, or to confirm to delete a handling. Gonna add a settings system for this, to make an option to remember your choice.

--- functions/saveload.lua
+++ functions/saveload.lua
@@ -8,40 +8,48 @@
 --|| YOU ARE NOT ALLOWED TO MAKE MIRRORS OR RE-RELEASES OF THIS SCRIPT WITHOUT PERMISSION FROM THE OWNERS
 --|| ***************************************************************************************************************** ]]
 
-function saveClient (  )
-    local sName = guiGetText ( menuContent["saveclient"].nameEdit )
+function trySave ( )
+    local sName       = guiGetText ( menuContent["saveclient"].nameEdit )
     local description = "description" --guiGetText ( menuContent["saveclient"].descriptionEdit )
-    if xmlSavesNode and sName and sName ~= "" and description and description ~= "" then
+    if sName and sName ~= "" and description and description ~= "" then
         local lName = string.lower ( sName )
-        local model = tostring ( getElementModel ( pVeh ) )
-        if not xmlSavesTable[lName] then
-            xmlSavesTable[lName]       = {h={}}
-            xmlSavesTable[lName].m     = model
-            xmlSavesTable[lName].s     = sName
-            xmlSavesTable[lName].d     = description
-            xmlSavesTable[lName].sNode = xmlCreateChild ( xmlSavesNode,               "save"     )
-            xmlSavesTable[lName].hNode = xmlCreateChild ( xmlSavesTable[lName].sNode, "handling" )
-        end
-        xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "model",       model )
-        xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "name",        sName )
-        xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "description", description )
-        for p,v in pairs ( getVehicleHandling ( pVeh ) ) do
-            xmlSavesTable[lName].h[p] = handlingToString ( v )
-            xmlNodeSetAttribute ( xmlSavesTable[lName].hNode, p, handlingToString ( v ) )
-        end
-        xmlSaveFile ( xmlSavesNode )
-        reloadClientSaves ( )
-        showData ( pm )
-        outputHandlingLog ( clog.saved, 0 )
-    else outputHandlingLog ( clog.invalidSave, 2 )
-    end
+        if xmlSavesTable[lName] then guiCreateWarningMessage ( text.askToReplace, 1, {saveClient,sName,lName,description} )
+        else saveClient ( sName, lName ) end
+    else outputHandlingLog ( clog.invalidSave, 2 ) end
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 --//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
 --------------------------------------------------------------------------------------------------------------------------
 
-function deleteHandling ( )
+function saveClient ( sName, lName, description )
+    local model = tostring ( getElementModel ( pVeh ) )
+    if not xmlSavesTable[lName] then
+        xmlSavesTable[lName]       = {h={}}
+        xmlSavesTable[lName].m     = model
+        xmlSavesTable[lName].s     = sName
+        xmlSavesTable[lName].d     = description
+        xmlSavesTable[lName].sNode = xmlCreateChild ( xmlSavesNode,               "save"     )
+        xmlSavesTable[lName].hNode = xmlCreateChild ( xmlSavesTable[lName].sNode, "handling" )
+    end
+    xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "model",       model )
+    xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "name",        sName )
+    xmlNodeSetAttribute ( xmlSavesTable[lName].sNode, "description", description )
+    for p,v in pairs ( getVehicleHandling ( pVeh ) ) do
+        xmlSavesTable[lName].h[p] = handlingToString ( v )
+        xmlNodeSetAttribute ( xmlSavesTable[lName].hNode, p, handlingToString ( v ) )
+    end
+    xmlSaveFile ( xmlSavesNode )
+    reloadClientSaves ( )
+    showData ( pm )
+    outputHandlingLog ( clog.saved, 0 )
+end
+
+--------------------------------------------------------------------------------------------------------------------------
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
+--------------------------------------------------------------------------------------------------------------------------
+
+function tryDelete ( )
     if     guiGetVisible ( menuContent["saveclient"].grid ) then grid = menuContent["saveclient"].grid
     elseif guiGetVisible ( menuContent["loadclient"].grid ) then grid = menuContent["loadclient"].grid
     end
@@ -50,12 +58,21 @@ function deleteHandling ( )
         if row > -1 and col > -1 then
             local name = string.lower ( guiGridListGetItemText ( grid, row, col ) )
             if name and xmlSavesTable[name] then
-                xmlDestroyNode ( xmlSavesTable[name].sNode )
-                xmlSavesTable[name] = {}
-                reloadClientSaves ( )
+                guiCreateWarningMessage ( text.askToDelete, 1, {deleteHandling,name} )
             end
         end
     end
+end
+
+--------------------------------------------------------------------------------------------------------------------------
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
+--------------------------------------------------------------------------------------------------------------------------
+
+function deleteHandling ( name )
+    xmlDestroyNode ( xmlSavesTable[name].sNode )
+    outputHandlingLog ( string.format ( clog.deletedHandling, xmlSavesTable[name].s ), 0 )
+    xmlSavesTable[name] = {}
+    reloadClientSaves ( )
 end
 
 --------------------------------------------------------------------------------------------------------------------------
@@ -67,9 +84,10 @@ function loadSavesIntoGrid ( gridList )
         guiGridListClear ( gridList )
         for n,v in pairs ( xmlSavesTable ) do
             if xmlSavesTable[n].sNode then
-                local row = guiGridListAddRow ( gridList )
+                local row   = guiGridListAddRow ( gridList )
+                local model = getVehicleNameFromModel ( tonumber ( xmlSavesTable[n].m ) )
                 guiGridListSetItemText ( gridList, row, 1, xmlSavesTable[n].s, false, false )
-                guiGridListSetItemText ( gridList, row, 2, xmlSavesTable[n].m, false, false )
+                guiGridListSetItemText ( gridList, row, 2, model,              false, false )
             end
         end
     end
