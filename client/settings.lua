cda9780bb0bfb111f98b5208c54c817583e6c18c
rdg94@live.nl <rdg94@live.nl@fe56017e-0f65-7fe6-02e5-73367f534cf0>
2011-12-17 09:39:59 +0000

    Added 1.2 notification that your handlings are not present, especially for the 1.2 beta test today.

--- client/settings.lua
+++ client/settings.lua
@@ -7,5 +7,6 @@ setting = {
     version = tostring ( EDITOR_REVISION ),
     minVersion = tostring ( EDITOR_MINIMUM_REVISION ),
     notifyUpdate = "false",
-    notifyUpgrade = "false"
+    notifyUpgrade = "false",
+    notifyHandlingDelete = "true"
 }
