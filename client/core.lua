9f319ae519f0c883e7aaaf04ccc014e2aaa2ce16
JohnMichaelFFS <JohnMichaelFFS@fe56017e-0f65-7fe6-02e5-73367f534cf0>
2011-12-26 05:39:16 +0000

    Addendum to r157

--- client/core.lua
+++ client/core.lua
@@ -1,63 +1,37 @@
-addEventHandler ( "onClientResourceStart", resourceRoot,
-    function ( )
-
-        local xml = xmlLoadFile(client_config_file) or xmlCreateFile(client_config_file, "config")
-        
-		if DEBUGMODE then
-			outputDebugString(string.format("fileExists = %s, xml root node = %s", tostring(fileExists(client_config_file)), tostring(xml)))
+--
+--	CLIENTSIDE RESOURCE START PROCEDURE
+--
+local function resourceStart()
+	--Load the clientside configuration file, or create a new one if it does not exist.
+	local rootNode = xmlLoadFile(client_config_file) or xmlCreateFile(client_config_file, "config")
+	
+	--Ensure all important setting nodes exist.
+	for settingKey, defaultValue in pairs(setting) do
+		if not xmlFindChild(rootNode, settingKey, 0) then
+			local newNode = xmlCreateChild(rootNode, settingKey)
+			xmlNodeSetValue(newNode, tostring(defaultValue))
 		end
-		
-        for cfg,value in pairs ( setting ) do
-            if not xmlFindChild ( xml, cfg, 0 ) then
-                local node = xmlCreateChild ( xml, cfg )
-                xmlNodeSetValue ( node, tostring ( value ) )
-				if DEBUGMODE then
-					outputDebugString(string.format("Added entry (%s, %s) [node = %s] to the client config file.", tostring(cfg), tostring(value), tostring(node)))
-				end
-            end
-        end
-        --xmlSaveFile(xml)
-		outputDebugString(tostring(#xmlNodeGetChildren(xml)))
-		
-        local ver = getUserConfig ( "version" )
-        --[[if ver and ver < HVER then -- Prevent older versions of hedit removing your new settings
-			for _,node in ipairs(xmlNodeGetChildren(xml)) do
-                local name = xmlNodeGetName ( node )
-                if not setting[name] then
-                    xmlDestroyNode ( node )
-                end
-            end
+	end
+	
+	xmlSaveFile(rootNode)
+	xmlUnloadFile(rootNode)
 
-        end]]
+	--Cache the client-side handling saves.
+	cacheClientSaves()
 
-        xmlSaveFile ( xml )
-        xmlUnloadFile ( xml )
+	--Query the server for admin rights.
+	triggerServerEvent ( "requestRights", root )
+	
+	--Build the GUI.
+	startBuilding ( )
+end
+addEventHandler("onClientResourceStart", resourceRoot, resourceStart)
 
-
-
-        cacheClientSaves ( )
-        
-        
-        
-        triggerServerEvent ( "requestRights", root )
-        
-        
-        
-        startBuilding ( )
-        return true
-    end
-)
-
-
-
-
-
-addEventHandler ( "onClientResourceStop", resourceRoot,
-    function ( )
-        
-        xmlUnloadFile ( client_handling_file )
-        
-        return true
-
-    end
-)
+--
+--	CLIENTSIDE RESOURCE STOP PROCEDURE
+--
+local function resourceStop()
+	--Unload the clientside configuration file.
+	xmlUnloadFile(client_handling_file)
+end
+addEventHandler("onClientResourceStop", resourceRoot, resourceStop)
